f27b74da23f13837c33708feef79f30e6a1d0088
abdullahalmomin <abd.momin003@gmail.com>
2019-05-09 16:02:55 +0600

    Homework done

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,103 @@
 const Todo = require("../models/Todo");
 
-module.exports.createTodo = (req, res) => {
+// create
+module.exports.createTodo = async (req, res) => {
   const todo = new Todo({
     task: req.body.task
   });
-  todo.save().then(doc => {
+  const newtodo = await todo.save();
+  res.json(newtodo);
+};
+
+// list
+
+module.exports.todoList = async (req, res) => {
+  const todos = await Todo.find();
+  if (todos.length === 0)
+    return res.status(404).json({
+      message: "no todo found"
+    });
+  res.send(todos);
+};
+// Delete
+module.exports.deleteTodo = async (req, res) => {
+  const { _id } = req.params;
+  const deleted = await Todo.findByIdAndDelete(_id);
+  if (deleted)
+    return res.json({
+      message: "todo deleted",
+      todo: deleted
+    });
+  res.status(404).json({
+    message: "not found or already deleted"
+  });
+};
+
+// update
+module.exports.updateTodo = async (req, res) => {
+  //   const todo = await Todo.findById(req.params._id);
+  //   todo.task = req.body.task;
+  //   todo.done = req.body.done;
+  //   await todo.save();
+
+  Todo.findByIdAndUpdate(
+    req.params._id,
+    {
+      $set: {
+        task: req.body.task,
+        done: req.body.done
+      }
+    },
+    { new: true }
+  ).then(doc => {
     res.json(doc);
   });
 };
+
+// doneTrue
+module.exports.doneTrue = async (req, res) => {
+  const todo = await Todo.find({ done: true });
+  console.log(todo);
+
+  return res.json(todo);
+};
+
+// doneFalse
+
+module.exports.doneFalse = async (req, res) => {
+  const todo = await Todo.find({ done: false });
+  console.log(todo);
+
+  return res.json(todo);
+};
+
+// today
+module.exports.today = async (req, res) => {
+  const today = new Date();
+  // today.setHours(0, 0, 0);
+  today.setHours(6, 0, 0);
+
+  const todayEnd = new Date();
+  // todayEnd.setHours(23, 59, 59);
+  todayEnd.setHours(17, 59, 59);
+
+  const todo = await Todo.find({
+    done: false,
+    createdAt: { $gte: today, $lt: todayEnd }
+  });
+  console.log(today.toUTCString(), todayEnd.toUTCString());
+  return res.json(todo);
+};
+
+// modified
+module.exports.modified = async (req, res) => {
+  const todo = await Todo.find({ done: false });
+  const result = todo.filter(
+    item => item.createdAt.toUTCString() !== item.updatedAt.toUTCString()
+  );
+  console.log(todo);
+  return res.json(result);
+};
+// toggle
+
+module.exports.toggleTodo = (req, res) => {};
--- routes/todos.js
+++ routes/todos.js
@@ -1,28 +1,48 @@
 const Router = require("express").Router();
 // const todoController = require("../controllers/todoController");
 // or
-const { createTodo } = require("../controllers/todoController");
+const {
+  createTodo,
+  todoList,
+  deleteTodo,
+  updateTodo,
+  toggleTodo,
+  doneTrue,
+  doneFalse,
+  today,
+  modified
+} = require("../controllers/todoController");
+const Todo = require("../models/Todo");
 
 // List=>get
-Router.get("/", (req, res) => {
-  res.send("list of all todos");
-});
+Router.get("/", todoList);
+// Done=>true
+Router.get("/gettrue", doneTrue);
+// Done=>False
+Router.get("/getfalse", doneFalse);
+// Created at today
+Router.get("/today", today);
+// modified
+Router.get("/modified", modified);
 
 // Create->post
 Router.post("/", createTodo);
 
 // Single->get
-Router.get("/:_id", (req, res) => {
-  res.send("A todo has been found" + req.params._id);
+Router.get("/:_id", async (req, res) => {
+  const todo = await Todo.findById(req.params._id);
+  if (todo) return res.json(todo);
+  res.status(404).json({
+    message: "no todo found"
+  });
 });
 
 // Update->put
-Router.put("/:_id", (req, res) => {
-  res.send("a record updated" + req.params._id);
-});
+Router.put("/:_id", updateTodo);
 
 // Delete->delete
-Router.delete("/:_id", (req, res) => {
-  res.send("A record deleted" + req.params._id);
-});
+Router.delete("/:_id", deleteTodo);
+// toggle
+Router.post("/toggle/:_id", toggleTodo);
+
 module.exports = Router;
